25672b9340fba3df39fd2528f63db61f8477b0bf
mostafa <shrytzadh@gmail.com>
2023-11-04 13:03:01 +0100

    bug fix

--- app/Http/Controllers/Panel/InvoiceController.php
+++ app/Http/Controllers/Panel/InvoiceController.php
@@ -265,43 +265,41 @@ class InvoiceController extends Controller
 
     private function storeInvoiceProducts(Invoice $invoice, $request)
     {
-        if (!$request->products) {
-            return back();
-        }
+        if ($request->products) {
+            foreach ($request->products as $key => $product_id){
+                if ($request->status == 'paid' && $request->status != $invoice->status){
+                    // decrease product counts
 
-        foreach ($request->products as $key => $product_id){
-            if ($request->status == 'paid' && $request->status != $invoice->status){
-                // decrease product counts
+                    $product = Product::find($product_id);
+                    $properties = json_decode($product->properties);
+                    $product_exist = array_keys(array_column($properties, 'color'), $request->colors[$key]);
 
-                $product = Product::find($product_id);
-                $properties = json_decode($product->properties);
-                $product_exist = array_keys(array_column($properties, 'color'), $request->colors[$key]);
+                    if ($product_exist){
+                        $properties[$product_exist[0]]->counts -= $request->counts[$key];
+                        $changed_properties = json_encode($properties);
+                        $product->update(['properties' => $changed_properties]);
+                    }
 
-                if ($product_exist){
-                    $properties[$product_exist[0]]->counts -= $request->counts[$key];
-                    $changed_properties = json_encode($properties);
-                    $product->update(['properties' => $changed_properties]);
+                    $product->update(['total_count' => $product->total_count -= $request->counts[$key]]);
                 }
 
-                $product->update(['total_count' => $product->total_count -= $request->counts[$key]]);
+                $invoice->products()->attach($product_id, [
+                    'color' => $request->colors[$key],
+                    'count' => $request->counts[$key],
+                    'unit' => $request->units[$key],
+                    'price' => $request->prices[$key],
+                    'total_price' => $request->total_prices[$key],
+                    'discount_amount' => $request->discount_amounts[$key],
+                    'extra_amount' => $request->extra_amounts[$key],
+                    'tax' => $request->taxes[$key],
+                    'invoice_net' => $request->invoice_nets[$key],
+                ]);
+
             }
-
-            $invoice->products()->attach($product_id, [
-                'color' => $request->colors[$key],
-                'count' => $request->counts[$key],
-                'unit' => $request->units[$key],
-                'price' => $request->prices[$key],
-                'total_price' => $request->total_prices[$key],
-                'discount_amount' => $request->discount_amounts[$key],
-                'extra_amount' => $request->extra_amounts[$key],
-                'tax' => $request->taxes[$key],
-                'invoice_net' => $request->invoice_nets[$key],
-            ]);
-
         }
 
         $invoice->other_products()->delete();
-
+        
         if ($request->other_products){
             foreach ($request->other_products as $key => $product){
                 $invoice->other_products()->create([
