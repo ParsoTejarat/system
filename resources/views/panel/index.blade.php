3d20ba5275fd916ab5b959df6aa7df803997ce7a
mostafa <shrytzadh@gmail.com>
2023-11-25 08:32:41 +0330

    ...

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -75,21 +75,23 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                        <div class="card border mb-0">
-                            <div class="card-body">
-                                <div class="d-flex align-items-center mb-3">
-                                    <div>
-                                        <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
-                                            <i class="fa fa-shopping-cart"></i>
+                    @can('accountant')
+                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                            <div class="card border mb-0">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center mb-3">
+                                        <div>
+                                            <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
+                                                <i class="fa fa-shopping-cart"></i>
+                                            </div>
                                         </div>
+                                        <span class="font-size-13">فاکتور</span>
+                                        <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Invoice::where('status','invoiced')->count() }}</h2>
                                     </div>
-                                    <span class="font-size-13">فاکتور</span>
-                                    <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Invoice::where('status','invoiced')->count() }}</h2>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endcan
                     <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
                         <div class="card border mb-0">
                             <div class="card-body">
