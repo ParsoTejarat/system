6087a420d93a840e23a0510ea49d8f28aff936df
mostafa <shrytzadh@gmail.com>
2023-12-06 07:44:06 +0100

    remove bot

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BotController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Panel\ChatController;
 use App\Http\Controllers\Panel\CouponController;
