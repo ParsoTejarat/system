d06f6d5a2c52c23e12b9758ffa989eaaf320262a
parso <parsotejarat@gmail.com>
2024-07-09 11:16:53 +0330

    bug fix

--- database/migrations/2023_10_09_071625_create_invoices_table.php
+++ database/migrations/2023_10_09_071625_create_invoices_table.php
@@ -27,7 +27,7 @@ class CreateInvoicesTable extends Migration
             $table->text('address');
             $table->string('postal_code');
             $table->string('phone');
-            $table->enum('status',['invoiced','pending','return','order'])->default('pending');
+            $table->enum('status',['invoiced','pending','order'])->default('order');
             $table->unsignedInteger('discount')->comment('تخفیف نهایی');
             $table->longText('description')->nullable();
             $table->text('order_status_desc')->nullable();
