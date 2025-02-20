ef0fd956e355a88a296fc4481a58a56f3d504b07
MrAidin <aidinsotudeh@gmail.com>
2025-02-20 10:36:45 +0330

    some-edits-for-products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -98,6 +98,6 @@ class Product extends Model
 
     public function brand()
     {
-        return $this->belongsTo(Brand::class);
+        return $this->belongsTo(Brand::class,'brand_id');
     }
 }
