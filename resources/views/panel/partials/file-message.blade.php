ae7e3f3f6ebefdc42af7e0b43c857c7c70f7e99a
MrAidin <aidinsotudeh@gmail.com>
2025-03-02 15:14:26 +0330

    add-some-features

--- resources/views/panel/partials/file-message.blade.php
+++ resources/views/panel/partials/file-message.blade.php
@@ -4,15 +4,15 @@
 @if(in_array($file->type, ['jpg','jpeg','png','webp','svg','gif']))
     <ul class="w-100">
         <li class="flex-column justify-content-center align-items-center w-100">
-            <a href="{{ $file->path }}">
-                <img src="{{ $file->path }}" alt="image" class="w-100 p-2">
+            <a href="{{ env('API_BASE_URL').$file->path }}">
+                <img src="{{ env('API_BASE_URL').$file->path }}" alt="image" class="w-100 p-2">
                 <span>{{ $file->name }}</span>
             </a>
         </li>
     </ul>
 @else
     <div class="m-b-0 text-muted text-left media-file">
-        <a href="{{ $file->path }}" class="btn btn-outline-light row text-left align-items-center justify-content-center h-50 w-100" download="{{ $file->path }}">
+        <a href="{{ env('API_BASE_URL').$file->path }}" class="btn btn-outline-light row text-left align-items-center justify-content-center h-50 w-100" download="{{ env('API_BASE_URL').$file->path }}">
             <i class="fa fa-download font-size-18 m-r-10"></i>
             <div class="small">
                 <div class="mb-2">{{ $file->name }}</div>
