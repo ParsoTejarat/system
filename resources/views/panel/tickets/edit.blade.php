824f8c8b0780925c57a26d9f248405857b752efa
MrAidin <aidinsotudeh@gmail.com>
2025-03-03 11:45:44 +0330

    add-some-features

--- resources/views/panel/tickets/edit.blade.php
+++ resources/views/panel/tickets/edit.blade.php
@@ -148,12 +148,12 @@
                                 <a href="javascript:void(0)" class="text-reset">
                                     @if(auth()->id() == $ticket->sender->company_user_id)
                                         {{ $ticket->receiver->name.' '.$ticket->receiver->family }}
-                                        <br>
-                                        <span>{{ getCompany($ticket->receiver->company_name) }}</span>
+{{--                                        <br>--}}
+{{--                                        <span>{{ getCompany($ticket->receiver->company_name) }}</span>--}}
                                     @else
                                         {{ $ticket->sender->name.' '.$ticket->sender->family }}
-                                        <br>
-                                        <span>{{ getCompany($ticket->sender->company_name) }}</span>
+{{--                                        <br>--}}
+{{--                                        <span>{{ getCompany($ticket->sender->company_name) }}</span>--}}
                                     @endif
                                 </a>
                             </h5>
