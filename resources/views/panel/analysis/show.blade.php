ed7ae52f8cc222c09adb0a4c89353580861ed093
MrAidin <aidinsotudeh@gmail.com>
2025-04-16 12:38:57 +0330

    edit-some-changes

--- resources/views/panel/analysis/show.blade.php
+++ resources/views/panel/analysis/show.blade.php
@@ -76,7 +76,7 @@
                                                 <td>
                                                     <a href="/panel/orders?code={{$analysis->order->code??'-'}}">{{ $analysis->order->code??'-' }}</a>
                                                 </td>
-                                                @dd($analysis->product->name)
+                                                @dd($analysis->product)
                                                 <td>{{ $analysis->product->name }}</td>
                                                 <td>{{ $analysis->count }}</td>
                                                 <td>{{  number_format($product->Inventory) }}</td>
