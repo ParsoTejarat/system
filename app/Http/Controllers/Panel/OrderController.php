4172aad9de5c5e6f0a2b44077d6c90e1c3a74495
MrAidin <aidinsotudeh@gmail.com>
2025-04-20 11:09:36 +0330

    edit-some-changes

--- app/Http/Controllers/Panel/OrderController.php
+++ app/Http/Controllers/Panel/OrderController.php
@@ -550,7 +550,7 @@ class OrderController extends Controller
         } else {
             $statuses = CustomerOrderStatus::ORDER_OTHER;
         }
-        dd($statuses);
+        dd($statuses,$order->type);
 
         $statusData = [];
 
